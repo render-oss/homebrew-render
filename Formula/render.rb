537a66eaf2d35ea63ebd76e60dc68bbfbd4830ee
Ed Ropple <ed@edropple.com>
2023-01-04 18:06:07 -0500

    render 0.1.2 (new formula)
    squashing 'new-formula/render' for pr-pull
    
    Closes #1.
    
    Signed-off-by: Ed Ropple <ed+github@edropple.com>

---
+++ Formula/render.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class Render < Formula
+  # the tag of the current release
+  RENDER_VERSION = "v0.1.2"
+  RENDER_REVISION = "a81b1b3b028440ff82950f36c80bdebaa3a49fe6"
+
+  RENDER_VERSION_OUTPUT = "#{RENDER_VERSION}-#{RENDER_REVISION[0..6]}-homebrew"
+
+  desc "Launch your next project into the cloud"
+  homepage "https://render.com"
+  url "https://github.com/render-oss/render-cli.git",
+      tag:      RENDER_VERSION,
+      revision: RENDER_REVISION
+  license "Apache-2.0"
+  head "https://github.com/render-oss/render-cli.git",
+       branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "deno" => :build
+
+  def install
+    # writes out a `-homebrew` version
+    inreplace "./version.ts" do |s|
+      s.gsub!("git-tree", RENDER_VERSION_OUTPUT)
+    end
+
+    # implies build-local
+    system "make", "build-completions"
+
+    bin.install "./bin/render"
+
+    bash_completion.install "./share/bash/bash_completion.d/render.bash"
+    zsh_completion.install "./share/zsh/site-functions/render.zsh"
+    fish_completion.install "./share/fish/vendor_completions.d/render.fish"
+  end
+
+  test do
+    assert_match RENDER_VERSION_OUTPUT, shell_output("#{bin}/render version")
+  end
+end
