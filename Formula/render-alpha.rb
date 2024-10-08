6365372de5edba151b1b29494e3e4acd783761a0
Jake Malachowski <5766239+jakemalachowski@users.noreply.github.com>
2024-10-08 17:05:04 -0500

    Update render-alpha.rb

--- Formula/render-alpha.rb
+++ Formula/render-alpha.rb
@@ -2,17 +2,15 @@ class RenderAlpha < Formula
   desc "Command-line interface for Render (Alpha version)"
   homepage "https://github.com/render-oss/render-cli-alpha"
   url "https://github.com/render-oss/render-cli-alpha.git",
-      tag:      "v0.1.0",
-      revision: "325534331837501ecfa6500df84ae823fc26a4b8"
+      tag:      "v0.2.0",
+      revision: "940a2415f419279dbdc7a601f8567841e41b333c"
   license "Apache-2.0"
-  
   depends_on "go" => :build
-
   def install
-    system "go", "build", "-o", bin/"render", *std_go_args(ldflags: "-s -w")[1..]
+    system "go", "build", *std_go_args(ldflags: "-s -w")[1..]
   end
 
   test do
-    assert_match "Usage:", shell_output("#{bin}/render --help")
+    assert_match "Usage:", shell_output("#{bin}/render-alpha --help")
   end
 end
