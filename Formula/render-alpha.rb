219d5df2005f590d1a24ab4ca719a7dda3edce09
Max Eshleman <max@render.com>
2024-11-06 11:49:19 -0700

    build with cli version

--- Formula/render-alpha.rb
+++ Formula/render-alpha.rb
@@ -1,13 +1,14 @@
 class RenderAlpha < Formula
+  def version = "0.6.0"
   desc "Command-line interface for Render (Alpha version)"
   homepage "https://github.com/render-oss/render-cli-alpha"
   url "https://github.com/render-oss/render-cli-alpha.git",
-      tag:      "v0.6.0",
+      tag:      "v"+version,
       revision: "99d89d88172648036c7ab094387da40801e6ee57"
   license "Apache-2.0"
   depends_on "go" => :build
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")[1..]
+    system "go", "build", *std_go_args(ldflags: "-s -w -X 'github.com/renderinc/render-cli/pkg/cfg.Version=#{version}'")[1..]
   end
 
   test do
