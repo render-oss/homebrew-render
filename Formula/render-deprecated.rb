e38cedba529ec401113789d6801ef07a8d67a5cf
Max Eshleman <max@render.com>
2024-11-11 15:31:25 -0700

    rename render-alpha to render (#26)

---
+++ Formula/render-deprecated.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class RenderDeprecated < Formula
+  # the tag of the current release
+  RENDER_VERSION  = "v0.1.11"
+  RENDER_REVISION = "3b0f049d839b2ad9804253f324034eeff58daefa"
+
+  RENDER_VERSION_OUTPUT = [
+    RENDER_VERSION,
+    RENDER_REVISION[0..6],
+    "homebrew",
+  ].map(&:chomp).join("-").freeze
+
+  desc "Launch your next project into the cloud"
+  homepage "https://render.com"
+  url "https://github.com/render-oss/render-cli-deprecated.git",
+      tag:      RENDER_VERSION,
+      revision: RENDER_REVISION
+  license "Apache-2.0"
+  head "https://github.com/render-oss/render-cli-deprecated.git",
+       branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/render-oss/homebrew-render/releases/download/render-0.1.11"
+    sha256 cellar: :any_skip_relocation, monterey: "62e41c8c43d5aa26f7f1376199a3c9eba4bce0fc81bbe40adc1ab09f0b18ec52"
+  end
+
+  deprecate! date: "2024-11-11", because: "New CLI available at https://github.com/render-oss/cli"
+
+  on_macos do
+    depends_on "deno" => :build
+  end
+
+  def install
+    # writes out a `-homebrew` version
+    inreplace "./version.ts" do |s|
+      s.gsub!("git-tree", RENDER_VERSION_OUTPUT)
+    end
+
+    # implies build-local
+    system "make", "build-completions"
+
+    bin.install "./bin/render-deprecated"
+
+    bash_completion.install "./share/bash/bash_completion.d/render.bash"
+    zsh_completion.install "./share/zsh/site-functions/render.zsh"
+    fish_completion.install "./share/fish/vendor_completions.d/render.fish"
+  end
+
+  test do
+    assert_match RENDER_VERSION_OUTPUT, shell_output("#{bin}/render-deprecated version")
+  end
+end
--- Formula/render.rb
+++ Formula/render.rb
@@ -1,56 +1,19 @@
-# frozen_string_literal: true
-
 class Render < Formula
-  # the tag of the current release
-  RENDER_VERSION = "v0.1.11"
-  RENDER_REVISION = "3b0f049d839b2ad9804253f324034eeff58daefa"
-
-  RENDER_VERSION_OUTPUT = [
-    RENDER_VERSION,
-    RENDER_REVISION[0..6],
-    "homebrew",
-  ].map(&:chomp).join("-").freeze
-
-  desc "Launch your next project into the cloud"
-  homepage "https://render.com"
-  url "https://github.com/render-oss/render-cli.git",
-      tag:      RENDER_VERSION,
-      revision: RENDER_REVISION
+  version = "0.7.0"
+  tag     = "v#{version}"
+  desc "Command-line interface for Render (Beta version)"
+  homepage "https://github.com/render-oss/cli"
+  url "https://github.com/render-oss/cli.git",
+      tag:      tag,
+      revision: "7ec41f67586cc58baf9d29f5045d927d2bff7a8e"
   license "Apache-2.0"
-  head "https://github.com/render-oss/render-cli.git",
-       branch: "main"
-
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
-
-  bottle do
-    root_url "https://github.com/render-oss/homebrew-render/releases/download/render-0.1.11"
-    sha256 cellar: :any_skip_relocation, monterey: "62e41c8c43d5aa26f7f1376199a3c9eba4bce0fc81bbe40adc1ab09f0b18ec52"
-  end
-
-  on_macos do
-    depends_on "deno" => :build
-  end
+  depends_on "go" => :build
 
   def install
-    # writes out a `-homebrew` version
-    inreplace "./version.ts" do |s|
-      s.gsub!("git-tree", RENDER_VERSION_OUTPUT)
-    end
-
-    # implies build-local
-    system "make", "build-completions"
-
-    bin.install "./bin/render"
-
-    bash_completion.install "./share/bash/bash_completion.d/render.bash"
-    zsh_completion.install "./share/zsh/site-functions/render.zsh"
-    fish_completion.install "./share/fish/vendor_completions.d/render.fish"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X 'github.com/renderinc/render-cli/pkg/cfg.Version=#{version}'")[1..]
   end
 
   test do
-    assert_match RENDER_VERSION_OUTPUT, shell_output("#{bin}/render version")
+    assert_match "Usage:", shell_output("#{bin}/render-alpha --help")
   end
 end
