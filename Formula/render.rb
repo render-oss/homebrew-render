ae5618388561c6707cac6b2dddf032c6c1b1a705
Max Eshleman <max@render.com>
2025-01-30 10:22:35 -0700

    rename renderinc to render-oss (#36)
    * rename renderinc to render-oss
    
    * update render cli version

--- Formula/render.rb
+++ Formula/render.rb
@@ -1,8 +1,8 @@
 class Render < Formula
   desc "Command-line interface for Render"
   homepage "https://github.com/render-oss/cli"
-  url "https://github.com/render-oss/cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "bbc1a37e541c2bbfcb96bbff043fe4f9bcb1e80c70a5224639f1e0a99c0e2cc1"
+  url "https://github.com/render-oss/cli/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "c68166aab1f2160be2ad7662e9174498235d5a345d54c6a0588a3ac835dcc5ea"
   license "Apache-2.0"
   head "https://github.com/render-oss/cli.git", branch: "main"
 
@@ -16,7 +16,7 @@ class Render < Formula
   def install
     ldflags = %W[
       -s -w
-      -X github.com/renderinc/cli/pkg/cfg.Version=#{version}
+      -X github.com/render-oss/cli/pkg/cfg.Version=#{version}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags)
   end
--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Or `brew tap render-oss/render` and then `brew install <formula>`.
 ## Documentation
 
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+## Updating Render cli version
+
+1. Update `url` in `render.rb` to point to the new version
+2. Update `sha256` in `render.rb` to the sha of the tar file the url points to (`shasum -a 256 ./cli-1.1.1.tar.gz`)
